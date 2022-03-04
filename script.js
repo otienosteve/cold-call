0d2e5665670814ea3a63975e6a8923fa90072646
otienosteve <steveotieno701@gmail.com>
2022-03-04 10:03:28 +0300

    The beginning

---
+++ script.js
@@ -0,0 +1,51 @@
+const students=["Paullete Adhiambo",
+"Adlight Akinyi",
+"Damaris Chepchirchir",
+"Flora	Chepchirchir",
+"Hanan	Ibrahim",
+"Denis	Kagunda",
+"Rose	kamau",
+"Isabel	Karimi",
+"Kabui	Kariuki",
+"Richard Koinange",
+"Sam Kuria",
+"Catherine Marite",
+"Pauline Momanyi",
+"Martin	Muchai",
+"John Muiruri",
+"Eunice	Mungai",
+"Milcah	Muraguri",
+"Yvonne	Muthui",
+"Ann Mwangi",
+"Nancy Mwende",
+"Ann Ndegwa",
+"Lynn Nyangon",
+"Beverly Odhiambo",
+"John	Odhiambo",
+"John	Onyaango",
+"Joseph	Onyango",
+"Catherine Otondi",
+"William Owalla",
+"Ruth Eva Wamuyu",
+"Purity	Wanjiku",
+"Andrian Weru"]
+// console.log(students.length)
+const button=document.getElementById('generate');
+button.addEventListener('click',()=>{
+let randomIndex=Math.floor(Math.random()*100);
+let total=students.length
+
+para=document.getElementById('stud')
+    if(randomIndex>30){
+        let tens=randomIndex.toString().split('').map((elem)=>parseInt(elem)).reduce((ac,val)=>ac+=val,0)
+        
+        para.textContent=students[tens];
+        
+        }else{
+            para.textContent=students[randomIndex];
+        }
+
+
+})
+
+
